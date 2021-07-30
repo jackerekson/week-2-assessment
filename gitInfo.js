ac82fad8b4c22e6df2ccdab188a98b223fff686b
jackerekson <erekson.jack@gmail.com>
2021-07-30 11:13:49 -0500

    adding gitinfo.js

---
+++ gitInfo.js
@@ -0,0 +1,97 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+const gitDefinition = 'Git is a version contrl system that allows individuals and teams to track changes to code projects, helps prevent bugs and loss of progress and allows you to view the long history of a project in snapshots'
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+const gitHubDefinition = 'Github is a place for you to store all of your git repositories.'
+//////////////////PROBLEMS 3 - 8////////////////////
+/*
+    For the next several problems you will be creating objects containing information about different git 
+    commands.  Each object should contain 'description' and 'code' properties.  The 'description' property 
+    will be a string with a description of what that git command does.  The 'code' property should be a 
+    string of the actual command (what you type into your terminal).
+*/
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create an object called 'init' with 'description' and 'code' properties 
+    following the guidelines above to describe the init command.
+*/
+
+//CODE HERE
+const init = {
+    description: 'init, initilizes a local reposiotry in a folder',
+    code: 'git init'
+}
+//////////////////PROBLEM 4////////////////////
+/*
+    Create an object called 'clone' with 'description' and 'code' properties 
+    following the guidelines above to describe the clone command.
+*/
+
+//CODE HERE
+const clone = {
+    description: 'close, clones a repository into a new directory',
+    code: 'git clone'
+}
+//////////////////PROBLEM 5////////////////////
+/*
+    Create an object called 'status' with 'description' and 'code' properties 
+    following the guidelines above to describe the status command.
+*/
+
+//CODE HERE
+const status = {
+    description: "status, displays the stat of the working directory and the staging area. it lets  you see which changes have been staged, which haven't, and which files area't being tacked by git",
+    code: 'git status'
+}
+//////////////////PROBLEM 6////////////////////
+/*
+    Create an object called 'add' with 'description' and 'code' properties 
+    following the guidelines above to describe the add command.
+*/
+
+//CODE HERE
+const add = {
+    description: 'add, adds file contents to the index',
+    code: 'git add'
+}
+//////////////////PROBLEM 7////////////////////
+/*
+    Create an object called 'commit' with 'description' and 'code' properties
+    following the guidelines above to describe the commit command.
+*/
+
+//CODE HERE
+const commit = {
+    description: 'commit, commits the file or files that have been added to the repository, you will add a comment to the commit using -m""',
+    code: 'git commit'
+    '
+}
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create an object called 'push' with 'description' and 'code' properties 
+    following the guidelines above to describe the push command.
+*/
+
+//CODE HERE
+const push = {
+    description: 'push, updates remote refs along with associated objects',
+    code 'git push'
+}
